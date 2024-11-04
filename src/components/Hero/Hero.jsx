046ae9ac19e02fd43bbaff8538861bc384101fbc
Tribhuvan nath <tribhuvannath4567@gmail.com>
2024-11-04 09:32:33 +0530

    Uploaded resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,13 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/assets/skills/Resume-Bzs8Nt7N.pdf";
+    link.download = "Resume-Bzs8Nt7N.pdf";
+    link.click();
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -11,15 +18,9 @@ export const Hero = () => {
           I'm a full-stack developer with 5 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <a
-          href="/assets/skills/Resume-Bzs8Nt7N.pdf"
-          download
-          target="_blank" // Open in a new tab
-          rel="noopener noreferrer" // Secure linking
-          className={styles.contactBtn}
-        >
+        <button onClick={handleDownload} className={styles.contactBtn}>
           Download Resume
-        </a>
+        </button>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
