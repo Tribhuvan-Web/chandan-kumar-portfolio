ffea334a3304631060fc3ddd26a3c09481f06202
Tribhuvan nath <tribhuvannath4567@gmail.com>
2024-11-04 09:53:22 +0530

    Uploaded resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { FiArrowDownCircle } from "react-icons/fi";
 
 export const Hero = () => {
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href = "/assets/Resume-Bzs8Nt7N.pdf";
-    link.download = "Resume-Bzs8Nt7N.pdf";
-    link.click();
-  };
-
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -18,9 +12,16 @@ export const Hero = () => {
           I'm a full-stack developer with 5 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <button onClick={handleDownload} className={styles.contactBtn}>
-          Download Resume
-        </button>
+        <a
+          download="Resume.pdf"
+          href="/files/Resume.pdf"
+          className="font-general-medium flex p-3 justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-teal-700 text-gray-800 hover:text-white duration-500"
+          aria-label="Download Resume"
+        >
+          <span className="text-sm sm:text-lg font-general-medium duration-100">
+            Download Resume
+          </span>
+        </a>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
